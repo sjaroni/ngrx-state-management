6561bf2132b60bc11ed28fc5594189b7b2d95781
Stefan Jaroni <sjaroni@outlook.de>
2025-06-27 17:47:10 +0200

    install NgRX DevToolkit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { counterReducer } from './counter/states/counter.reducer';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideStore({ counter: counterReducer }),
-  ],
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+],
 };
