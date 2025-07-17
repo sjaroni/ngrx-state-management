ee57dac30b733887f745ee19a67c29065607f521
Stefan Jaroni <sjaroni@outlook.de>
2025-07-17 19:39:56 +0200

    feat: add effect after login success

--- src/app/auth/states/auth.effects.ts
+++ src/app/auth/states/auth.effects.ts
@@ -1,16 +1,15 @@
 import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { loginStart, loginSuccess } from './auth.actions';
-import { exhaustMap, map } from 'rxjs/operators';
+import { exhaustMap, map, tap } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthEffect {
-  // constructor(private actions$: Actions) {}
-
   private actions$ = inject(Actions);
-  authService = inject(AuthService); // Assuming you have an AuthService to handle authentication
-
+  authService = inject(AuthService);
+  router = inject(Router);
 
   login$ = createEffect(() => {
     return this.actions$.pipe(
@@ -19,11 +18,19 @@ export class AuthEffect {
         return this.authService.login(action.email, action.password).pipe(
           map((data) => {
             // handle the user object here, e.g., return a success action
-            return loginSuccess({ user: data })
+            return loginSuccess({ user: data });
           })
         );
       })
-    )
-  })
+    );
+  });
 
+  loginRedirect$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(loginSuccess),
+      tap((action) => {
+        this.router.navigate(['/']);
+      })
+    );
+  }, { dispatch: false });
 }
